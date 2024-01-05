cb3654b0922e2d8c579bd5997b8ab3cbbcc9467c
Peng Xiao <xiaoquwl@gmail.com>
2024-01-06 00:36:42 +0100

    add output option

--- githubctl/main.py
+++ githubctl/main.py
@@ -1,4 +1,5 @@
 import os
+from enum import Enum
 
 import typer
 from rich import print
@@ -16,10 +17,22 @@ repo_app = typer.Typer()
 app.add_typer(repo_app, name="repo")
 
 
+class OutputOption(str, Enum):
+    json = "json"
+    csv = "csv"
+    table = "table"
+
+
 @repo_app.command(name="list", help="list user repository")
-def list_repos(user: str = typer.Option(..., "--user", "-u", help="github user name")):
-    repo = get_all_user_repositories(username=user)
-    print(repo)
+def list_repos(
+    user: str = typer.Option(..., "--user", "-u", help="github user name"),
+    output: OutputOption = typer.Option(
+        OutputOption.json, "--output", "-o", help="output format"
+    ),
+):
+    # repo = get_all_user_repositories(username=user)
+    # print(repo)
+    print(output)
 
 
 if __name__ == "__main__":
