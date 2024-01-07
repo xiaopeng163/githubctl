275812159dfb179f165cfc60da6da42248db4377
Peng Xiao <xiaoquwl@gmail.com>
2024-01-07 22:19:14 +0100

    add table output

--- githubctl/utils.py
+++ githubctl/utils.py
@@ -4,6 +4,8 @@ import sys
 import json
 
 from rich import print_json
+from rich.console import Console
+from rich.table import Table
 
 from options import OutputOption
 
@@ -18,3 +20,15 @@ def print_beauty(list_of_dict: List[dict], output: OutputOption):
 
     elif output == OutputOption.json:
         print_json(json.dumps(list_of_dict))
+
+    elif output == OutputOption.table:
+        table = Table()
+        headers = list_of_dict[0].keys()
+        for h in headers:
+            table.add_column(str(h))
+
+        for repo in list_of_dict:
+            table.add_row(*[str(r) for r in repo.values()])
+
+        console = Console()
+        console.print(table)
