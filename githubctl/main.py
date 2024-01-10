9db0466274ffbab0f8f28f8247cdd32a1f2820f0
Peng Xiao <xiaoquwl@gmail.com>
2024-01-10 22:31:36 +0100

    add query option

--- githubctl/main.py
+++ githubctl/main.py
@@ -3,6 +3,7 @@ import os
 import typer
 from rich import print
 from dotenv import load_dotenv
+import jmespath
 
 from github import get_all_user_repositories
 from utils import print_beauty
@@ -23,10 +24,13 @@ app.add_typer(repo_app, name="repo")
 def list_repos(
     user: str = typer.Option(..., "--user", "-u", help="github user name"),
     output: OutputOption = typer.Option(
-        OutputOption.json, "--output", "-o", help="output format"
+        OutputOption.table, "--output", "-o", help="output format"
     ),
+    query: str = typer.Option(None, "--query", "-q", help="query with jmespath"),
 ):
     repo = get_all_user_repositories(username=user)
+    if query:
+        repo = jmespath.search(query, repo)
     print_beauty(list_of_dict=repo, output=output)
 
 
